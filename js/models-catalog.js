cf5578de963bf2ba148fcb257583d76164b31e02
JobYu <110712483+JobYu@users.noreply.github.com>
2025-04-26 16:25:36 +0800

    Update models-catalog.js

--- js/models-catalog.js
+++ js/models-catalog.js
@@ -7,25 +7,25 @@ const MODELS_CATALOG = [
         id: "test",
         title: "Test Model",
         filename: "models/test.vox",
-        description: "A simple test model to verify the system's functionality."
+        description: "A simple test model."
     },
     {
         id: "puff-shroom",
         title: "puff-shroom",
         filename: "models/puff-shroom.vox",
-        description: "Also known as Puff-shroom or Cannon Fodder Mushroom, a character from the tower defense strategy game series 'Plants vs. Zombies'."
+        description: "Also known as Puff-shroom or Cannon Fodder Mushroom."
     },
     {
         id: "angry_yellow",
         title: "Angry Bird Yellow ",
         filename: "models/angry_yellow.vox",
-        description: "An angry bird yellow model with distinctive facial expressions and a sleek design style."
+        description: "An angry bird yellow model."
     },
     {
         id: "angry_pig_xmas",
         title: "Christmas Angry Pig",
         filename: "models/angry_pig_Xmas.vox",
-        description: "A Christmas-themed angry pig character, combining holiday elements with a cute voxel style."
+        description: "A Christmas-themed angry bird pig character."
     }
 ];
 
